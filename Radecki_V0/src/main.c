735c23d48ca0fb53bdc4d26741ca4e084941e268
TheCao <komarek.zal@gmail.com>
2016-04-18 14:15:14 +0200

    ?

--- Radecki_V0/src/main.c
+++ Radecki_V0/src/main.c
@@ -9,59 +9,56 @@
 #include "armtimer.h"
 
 extern void BRANCHTO();
-extern void rand();
 extern void _enable_interrupts();
-void c_irq_handler(void)
-{
-	RPI_GetArmTimer()->IRQClear = 1;
-	uart_sendC("Interrupt");
-	hexstring(0x9999);
-}
+extern struct FrameBufferInfo fb_info;
+
 __attribute__((no_instrument_function))  VOID not_main(VOID)
 {
-
-
 	long as;
 	prepare_led_act();
 	prepare_led_pwr();
 	wait(DELAY_10_ms); // wymagane ze wzgledu na uart_sendC, ktore nie wyrabia po wyjsciu z funkcji
 	uart_init();
 
-
-
-	for(UINT32 i = 0; i < 10; i++)
-	{
-		led_act_ON();
-		led_pwr_ON();
-		wait(DELAY_100_ms);
-
-		led_act_OFF();
-		led_pwr_OFF();
-
-		wait(DELAY_100_ms);
-
-	}
-	uart_sendC("ARM Timer interrupts");
-	for(volatile UINT32 i = 0; i < 4 ; i++)
-		{
-			uart_sendC("W petli for");
-			hexstring(i);
-			wait(DELAY_1_s);
-		}
-
-	RPI_GetIrqController()->Enable_Basic_IRQs = RPI_BASIC_ARM_TIMER_IRQ;
+// ARM Timer Interrupt Controller
+	/*RPI_GetIrqController()->Enable_Basic_IRQs = RPI_BASIC_ARM_TIMER_IRQ;
 	arm_timer_init();
 	_enable_interrupts();
-	RPI_GetArmTimer()->Load = 0x3000;
-	uart_sendC("Wejscie w petle");
-	for(UINT32 i = 0; i < 10 ; i++)
-	{
-		uart_sendC("W petli for2");
-					hexstring(i);
-					wait(DELAY_1_s);
+*/
+
+	/*do{
+			as=InitialiseFrameBuffer();
 	}
-	arm_timer_stop();
-	//TODO: Sprawdziæ czy kompilacja dzia³a tak jak dzia³a ostatnio.
+	while (as!=0);
+	uart_sendC("FrameBuffer_Ready:");
+	uart_sendC("Pointer: ");
+	hexstring(as);
+	uart_sendC("FrameBuffer data (Pitch/Pointer/Height) :");
+	hexstring(fb_info.Pitch);
+	hexstring(fb_info.Pointer);
+	hexstring(fb_info.Height);
+
+	memset ( (void*)fb_info.Pointer, 0, 1920UL*1080UL*4);
+	for(as=0;as<1080;as++)
+	{*((long*)(fb_info.Pointer+(fb_info.Pitch*as)+as*4))=0x0ffffff;
+	*((long*)(fb_info.Pointer+(fb_info.Pitch*as)+(as+8)*4))=0x0000ff00;
+	*((long*)(fb_info.Pointer+(fb_info.Pitch*as)+(as+rand()%30)*4))=0x00000ff;
+	*((long*)(fb_info.Pointer+(fb_info.Pitch*as)+(as+rand()%130)*4))=0x0ff0000;
+	};
+*/
+	RPI_GetIrqController()->Enable_Basic_IRQs = RPI_BASIC_ARM_TIMER_IRQ;
+		arm_timer_init();
+		_enable_interrupts();
+		RPI_GetArmTimer()->Load = 0x3000;
+		uart_sendC("Wejscie w petle");
+		for(UINT32 i = 0; i < 10 ; i++)
+		{
+			uart_sendC("W petli for2");
+						hexstring(i);
+						wait(DELAY_1_s);
+		}
+		arm_timer_stop();
+
 	BRANCHTO(0x8000);
 
 
